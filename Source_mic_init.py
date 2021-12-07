ce32db6c9a28d479fcc05a9d5de72006ff22d255
Phantasmal-Groove <57569543+Phantasmal-Groove@users.noreply.github.com>
2021-12-07 22:22:28 +0100

    Revamped Source_mic_init (using RIR)
    Revamped initialization of the conference room geometry and room impulse generation. All the parameters still dynamically variable. One problem is that RIR doesnt allow mutliple sources in one initialization so I put it into a for loop (a messy approach). 
    
    The output is a 'signal' matrix with following indicies:
    [samples, stereo channel (not really needed), reciever index, source index]

--- Source_mic_init.py
+++ Source_mic_init.py
@@ -1,39 +1,27 @@
-#NumPy package
+import numpy as np
+import scipy.signal as ss
+import rir_generator as rir
+from scipy.io import wavfile
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-import numpy as np
-from scipy.io import wavfile
 
 #init values
 #Mowa.wav is 24-bit and is normalized to 0dBFS
 sample_rate_speech, speech_data = wavfile.read("recordings/mowa.wav")
 #computer_fan.wav is 24-bit and is normalized to -2dBFS
 sample_rate_noise, noise_data = wavfile.read("recordings/computer_fan.wav")
-fs = sample_rate_speech
-dt = 1/fs
-c = 343
-
 
 ###Geometry setup
-#mics in front of the table
-# [x, y]
-mic_d = 0.3
-mics = np.array([
-    [mic_d, 0],
-    [0, 0],
-    [-mic_d, 0]
-    ])
-mics_phi = 2*np.pi/360*np.array([0, 0, 0])
-
 #Rectangle room 7 m x 4.6 m
 room_d1 = 5
 room_d2 = 3
+room_d3 = 3
 mic_d_from_wall = 0.2
 room_corners = np.array([
-    [0-room_d2/2, -mic_d_from_wall],
-    [0-room_d2/2, room_d1-mic_d_from_wall],
-    [0+room_d2/2, room_d1-mic_d_from_wall],
-    [0+room_d2/2, -mic_d_from_wall]
+    [0, 0],
+    [0, room_d1],
+    [0+room_d2, room_d1],
+    [0+room_d2, 0]
 ])
 
 #Table in the middle of the room 3 m x 1.2 m
@@ -41,157 +29,102 @@ table_d_from_mic = 0.3
 table_d1 = 3
 table_d2 = 1.2
 table_corners = np.array([
-    [0-table_d2/2, table_d_from_mic],
-    [0-table_d2/2, table_d1+table_d_from_mic],
-    [0+table_d2/2, table_d1+table_d_from_mic],
-    [0+table_d2/2, table_d_from_mic]
+    [0-table_d2/2+room_d2/2, table_d_from_mic],
+    [0-table_d2/2+room_d2/2, table_d1+table_d_from_mic],
+    [0+table_d2/2+room_d2/2, table_d1+table_d_from_mic],
+    [0+table_d2/2+room_d2/2, table_d_from_mic]
 ])
+
+#mics in front of the table
+# [x, y]
+mic_d = 0.3
+mic_d_from_wall = 0.2
+mic_h = 1.2
+mics = np.array([
+    [mic_d+room_d2/2, mic_d_from_wall, mic_h],
+    [room_d2/2,  mic_d_from_wall, mic_h],
+    [-mic_d+room_d2/2,  mic_d_from_wall, mic_h]
+    ])
+mics_phi = 2*np.pi/360*np.array([0, 0, 0])
 
 #sources 0-6 are the seven seats around the table, 7-x are noise sources (for example AC or fan noise from a hardware rack)
 source_d_from_table = 0.1
+source_h = 1.2
 rack_d_from_wall = 0.3
 num_of_signal_sources = 6
 num_of_noise_sources = 1
 sources = np.array([
-    [table_corners[0][0]-source_d_from_table, table_corners[0][1]+1*table_d1/6],
-    [table_corners[0][0]-source_d_from_table, table_corners[0][1]+3*table_d1/6],
-    [table_corners[0][0]-source_d_from_table, table_corners[0][1]+5*table_d1/6],
-    [0, table_corners[1][1]+source_d_from_table],
-    [table_corners[2][0]+source_d_from_table, table_corners[3][1]+5*table_d1/6],
-    [table_corners[2][0]+source_d_from_table, table_corners[3][1]+3*table_d1/6],
-    [table_corners[2][0]+source_d_from_table, table_corners[3][1]+1*table_d1/6],
-    [room_corners[2][0]-rack_d_from_wall, room_corners[2][1]-rack_d_from_wall]
+    [table_corners[0][0]-source_d_from_table, table_corners[0][1]+1*table_d1/6, source_h],
+    [table_corners[0][0]-source_d_from_table, table_corners[0][1]+3*table_d1/6, source_h],
+    [table_corners[0][0]-source_d_from_table, table_corners[0][1]+5*table_d1/6, source_h],
+    [(table_corners[0][0]+table_corners[2][0])/2, table_corners[1][1]+source_d_from_table, source_h],
+    [table_corners[2][0]+source_d_from_table, table_corners[3][1]+5*table_d1/6, source_h],
+    [table_corners[2][0]+source_d_from_table, table_corners[3][1]+3*table_d1/6, source_h],
+    [table_corners[2][0]+source_d_from_table, table_corners[3][1]+1*table_d1/6, source_h],
+    [room_corners[2][0]-rack_d_from_wall, room_corners[2][1]-rack_d_from_wall, 2*source_h]
 ])
 
 
 
-###geometry visualization
-# fig, ax1 = plt.subplots(figsize=(8, 8))
-# source_labels = range(0,np.size(sources, axis=0))
-# ax1.plot(mics[:, 0], mics[:, 1], 'ko', label='Microphones')
-# for i in range(0,np.size(mics, axis=0)):
-#     plt.annotate(i,
-#         (mics[i, 0],mics[i, 1]),
-#         textcoords="offset points", 
-#         xytext=(0,-12), 
-#         ha='center')
+##geometry visualization
+fig, ax1 = plt.subplots(figsize=(8, 8))
+source_labels = range(0,np.size(sources, axis=0))
+ax1.plot(mics[:, 0], mics[:, 1], 'ko', label='Microphones')
+for i in range(0,np.size(mics, axis=0)):
+    plt.annotate(i,
+        (mics[i, 0],mics[i, 1]),
+        textcoords="offset points", 
+        xytext=(0,-12), 
+        ha='center')
 
-# ax1.plot(sources[0:7, 0], sources[0:7, 1], 'rx', label='Sources (seats)')
-# for i in range(0,num_of_signal_sources+1):
-#     plt.annotate(i,
-#         (sources[i, 0],sources[i, 1]),
-#         textcoords="offset points", 
-#         xytext=(0,5), 
-#         ha='center')
+ax1.plot(sources[0:7, 0], sources[0:7, 1], 'rx', label='Sources (seats)')
+for i in range(0,num_of_signal_sources+1):
+    plt.annotate(i,
+        (sources[i, 0],sources[i, 1]),
+        textcoords="offset points", 
+        xytext=(0,5), 
+        ha='center')
 
-# ax1.plot(sources[7, 0], sources[7, 1], 'kx', label='Sources (noise)')
-# for i in range(num_of_signal_sources+1, num_of_signal_sources+num_of_noise_sources+1):
-#     plt.annotate(i-num_of_signal_sources-1,
-#         (sources[i, 0],sources[i, 1]),
-#         textcoords="offset points", 
-#         xytext=(0,5), 
-#         ha='center')
+ax1.plot(sources[7, 0], sources[7, 1], 'kx', label='Sources (noise)')
+for i in range(num_of_signal_sources+1, num_of_signal_sources+num_of_noise_sources+1):
+    plt.annotate(i-num_of_signal_sources-1,
+        (sources[i, 0],sources[i, 1]),
+        textcoords="offset points", 
+        xytext=(0,5), 
+        ha='center')
 
-# ax1.add_patch(Rectangle((table_corners[0][0], table_corners[0][1]), table_d2, table_d1,color="brown", edgecolor="black",label='Table'))
-# ax1.add_patch(Rectangle((room_corners[0][0], room_corners[0][1]), room_d2, room_d1,fill=False,edgecolor="black",label='Room'))
-# ax1.grid()
-# ax1.legend()
-# ax1.set_xlim([-room_d2/2-0.7, room_d2/2+1])
-# ax1.set_ylim([-mic_d_from_wall-0.7, room_d1+mic_d_from_wall+1])
-# ax1.set_title("Geometry of the conference setup")
-# plt.show()
+ax1.add_patch(Rectangle((table_corners[0][0], table_corners[0][1]), table_d2, table_d1,color="brown", edgecolor="black",label='Table'))
+ax1.add_patch(Rectangle((room_corners[0][0], room_corners[0][1]), room_d2, room_d1,fill=False,edgecolor="black",label='Room'))
+ax1.grid()
+ax1.legend()
+ax1.set_xlim([-0.7, room_d2+1])
+ax1.set_ylim([-mic_d_from_wall-0.7, room_d1+mic_d_from_wall+1])
+ax1.set_title("Geometry of the conference setup")
+plt.show()
 
+#Generating impulse respons using RIR and convolving with the signal
+n_samples = 4096
+signal = np.zeros(shape=(np.size(speech_data,axis=0)+n_samples-1, 2, np.size(mics, axis=0), np.size(sources, axis=0)))
+for i in range(0,np.size(sources, axis=0)):
+    h = rir.generate(
+        c=343,                  # Sound velocity (m/s)
+        fs=sample_rate_speech,                  # Sample frequency (samples/s)
+        r=mics,
+        s=sources[i],    
+            # Source position [x y z] (m)
+        L=[room_d2, room_d1, room_d3],            # Room dimensions [x y z] (m)
+        reverberation_time=0.6, # Reverberation time (s)
+        nsample=n_samples,           # Number of output samples
+    )
+    # Convolve 2-channel signal with 3 impulse responses
+    signal[:,:,:,i] = ss.convolve(h[:, None, :], speech_data[:, :, None])
 
+#[samples, stereo channel, reciever index, source index]
+print(signal.shape) 
 
-###Propagation of the sound wave
-def compute_delay_and_attenuation(mic_coordinates, source_coordinates, c):
-    """
-    Parameters
-    ----------
-    mic_coordinates
-        Coordinates of the microphone [x,y]
-        
-    source_coordinates
-        Coordinates of the source [x,y]
-
-    c
-        Speed of sound [m/s]
-
-    Returns
-    -------
-    t_delay
-        Time delay for a specific microphone and source [s]
-
-    attenuation
-        A linear scalar by which the signal should be attenuated after transmission through the medium. It assumes a 24-bit depth wave was used as the signal.
-
-    """
-    distance = np.sqrt((source_coordinates[0] - mic_coordinates[0])**2+(source_coordinates[1] - mic_coordinates[1])**2)
-    t_delay = distance/c
-    dB_attenuation = 10*np.log10(distance**2)
-    attenuation = np.divide(2**24*10**(-dB_attenuation/20),2**24)
-    return t_delay, attenuation
-
-
-def compute_angle(mic_coordinates, source_coordinates):
-    """
-    Parameters
-    ----------
-    mic_coordinates
-        Coordinates of the microphone [x,y]
-
-    source_coordinates
-        Coordinates of the source [x,y]
-
-    Returns
-    -------
-    phi
-        Angle from x axis to line between mic and source
-
-    """
-    phi = np.arctan2((source_coordinates[1] - mic_coordinates[1]), (source_coordinates[0] - mic_coordinates[0]))
-    return phi
-
-def directivity_gain(angle):
-    """
-    :param angle: angle from front of microphone (cardioid) [rad]
-    :return: gain for sound from this direction [0-1]
-    """
-    return 0.5*(1 + np.cos(angle))
-
-
-
-###Calculated delay, axis=0 is the specific microphone, axis=1 is the specific source, t_delay is the time delay, p_delay is the phase delay
-t_delay = np.zeros(shape=(np.size(mics,axis=0), np.size(sources,axis=0)))
-attenuation = np.zeros(shape=(np.size(mics,axis=0), np.size(sources,axis=0)))
-for i in range(0,np.size(t_delay,axis=0)):
-    for j in range(0,np.size(t_delay,axis=1)):
-        t_delay[i,j], attenuation[i,j] = compute_delay_and_attenuation(mics[i],sources[j],c)
-
-
-
-
-###Speech data for mics. axis=0 is the mic number, axis=1 is the source number, axis=2 is the sample number. Already attenuated.
-s_delay = np.rint(t_delay*fs)
-speech_data_for_mics = np.zeros((np.size(mics,axis=0),np.size(sources,axis=0),np.size(speech_data,axis=0)+int(s_delay.max())))
-for i in range(0, np.size(speech_data_for_mics,axis=0)):
-    for j in range(0, np.size(speech_data_for_mics,axis=1)):
-        speech_data_for_mics[i,j,int(s_delay[i,j]):np.size(speech_data, axis=0)+int(s_delay[i,j])] += attenuation[i,j]*speech_data * directivity_gain(compute_angle(mics[i], sources[j]) + mics_phi[i])
-
-
-##Plot example of a specific signal in a microphone
-# fig, ax2 = plt.subplots(figsize=(10, 8))
-# ax2.plot(speech_data_for_mics[0,3,:],'r', label="mic=0, source=3, t_delay = " + str(t_delay[0,3]))
-# ax2.plot(speech_data_for_mics[1,3,:],'b',label="mic=1, source=3, t_delay = " + str(t_delay[1,3]))
-# ax2.set_title("Speech signal for two different mics")
-# ax2.grid()
-# ax2.legend()
-# ax2.set_xlim([15600, 16100])
-# plt.show()
-
-##Plot Example of attenuation
-fig, ax3 = plt.subplots(figsize=(10, 8))
-ax3.plot(speech_data,'r', label="Source 3 signal (1 m away)")
-ax3.plot(speech_data_for_mics[1,3,:],'b',label="mic=1, source=3 (3.3 m away)")
-ax3.legend()
+fig, ax2 = plt.subplots(figsize=(10, 8))
+ax2.plot(signal[:,0,1,0], 'r', label="reciever 1, source 0")
+ax2.plot(signal[:,0,1,3], 'b', label="reciever 1, source 3")
+ax2.plot(signal[:,0,1,5], 'g', label="reciever 1, source 5")
+ax2.legend()
 plt.show()
