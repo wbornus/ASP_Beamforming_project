b05c97fae2c60216c8963212a13486a338eae486
wiktor_bornus <wbornus@gmail.com>
2021-12-21 13:32:17 +0100

    polar response step

---
+++ polar_response_step.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def polar_response_step(x):
+    """
+    :param x: multichannel input (time domain) M x N, M-no channels, N-per channel time series
+    :return: multichannel output (time domain) with applied directivity mods
+    """
+
+
+    return NotImplemented
