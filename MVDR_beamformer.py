a688b2054a3e1583c7b839f02d97be7e7b219b83
123Xtom <40460710+123Xtom@users.noreply.github.com>
2022-01-03 21:08:32 +0100

    added mic directivity to beamformer

--- MVDR_beamformer.py
+++ MVDR_beamformer.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 import rir_generator as rir
 
 
-
-def MVDR_beamformer(x, distances, phi,  fs):
+def MVDR_beamformer(x, distances, phi, fs, mic_directivity="omni", rotations=None):
     """
     :param x: input time signals [M x N], M - number of microphones, N - number of samples
     :param distances: [0, d1, d2, ... dM] - distances between microphone 0 to mic i
     :param phi: direction of beam (scalar) [rad]
     :param fs: sampling frequency
+    :param mic_directivity: directivity of microphones - omni (default) / cardioid
+    :param rotations: rotation of each microphone in reference to front of matrix [rad]
     :return out: output signal
     """
     X = np.zeros([x.shape[0], len(np.fft.rfft(x[0, :]))]).astype(complex)
@@ -19,11 +20,19 @@ def MVDR_beamformer(x, distances, phi,  fs):
 
     # steering vector
     d = np.zeros([X.shape[0], X.shape[1]]).astype(complex)
+    # directivity-based weights
+    if rotations is None:
+        rotations = np.zeros(X.shape[0])
+    U = np.ones(d.shape)
+    if mic_directivity == "cardioid":
+        for i in range(X.shape[0]):
+            U[i, :] = 0.5*(1+np.cos(phi+rotations[i]))
+
     f = np.linspace(0, fs/2, X.shape[1])
     for i in range(X.shape[0]):
         tau = np.cos(phi) * distances[i] / c
         for j in range(X.shape[1]):
-            d[i, j] = np.exp(-1j*2*np.pi*f[j]*tau)
+            d[i, j] = np.exp(-1j*2*np.pi*f[j]*tau)*U[i, j]
 
     R = np.zeros([X.shape[0], X.shape[0], X.shape[1]]).astype(complex)
     for i in range(X.shape[1]):
@@ -72,7 +81,7 @@ def polar_pattern(mics, angle):
         distances = np.append(distances, np.sqrt((mics[0][0] - mics[i][0])**2 + (mics[0][1] - mics[i][1])**2))
     out_power = np.array([])
     phi_out = np.array([])
-    for phi in np.linspace(0, np.pi*2, 1000):
+    for phi in np.linspace(0, np.pi*2, 200):
         phi_out = np.append(phi_out, phi)
         source = [r*np.cos(phi) + 5, r*np.sin(phi)+5, height]
         # Generating impulse respons using RIR and convolving with the signal
@@ -90,8 +99,18 @@ def polar_pattern(mics, angle):
             reverberation_time=0.0,  # Reverberation time (s)
             nsample=n_samples,  # Number of output samples
             order=0,  # order of reflections
+            mtype=rir.mtype.cardioid,
+            orientation=[0, 0]
         )
-        power = np.sqrt(np.mean(MVDR_beamformer(h.T, distances, angle/360*2*np.pi, Fs)**2))
+        h_cor = h
+        h_cor[:, 1] = h[:, 1] * (0.5 * (1 + np.cos(phi + np.pi / 2)))
+        h_cor[:, 2] = h[:, 2] * (0.5 * (1 + np.cos(phi - np.pi / 2)))
+        power = np.sqrt(np.mean(MVDR_beamformer(h_cor.T,
+                                                distances,
+                                                angle/360*2*np.pi,
+                                                Fs,
+                                                mic_directivity="cardioid",
+                                                rotations=[0, np.pi/2, -np.pi/2])**2))
         out_power = np.append(out_power, power)
         print('angle: ', phi, power)
 
@@ -130,10 +149,10 @@ if __name__ == "__main__":
         [room_d2/2,  -mic_d+room_d1/2, mic_h]
         ])
 
-    [pattern, theta] = polar_pattern(mics, 0)
-    plt.plot(theta[250:750], 10*np.log10(pattern[250:750]/np.max(pattern)))
-    plt.show()
-
-    # fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    # ax.plot(theta, 10*np.log10(pattern/np.max(pattern)))
+    [pattern, theta] = polar_pattern(mics, 90)
+    # plt.plot(theta[250:750], 10*np.log10(pattern[250:750]/np.max(pattern)))
     # plt.show()
+
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    ax.plot(theta, 10*np.log10(pattern/np.max(pattern)))
+    plt.show()
